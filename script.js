c476a73b8cc5ceeabe1634a8d95566e431c26272
Chris Twaddell <ct9233@gmail.com>
2020-10-20 23:00:19 -0700

    Retract Nav on mouseleave

--- script.js
+++ script.js
@@ -2,18 +2,24 @@ const menuLogo = document.querySelector('.logo');
 const nav1 = document.getElementById('nav-1');
 const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
+const navMenu = document.getElementById('menu');
 const navItems = [nav1, nav2, nav3]
-const menuItems = document.querySelectorAll('.menu');
 
 function expandMenu() {
     if (nav1.classList.contains('inactive-1')) {
         navItems.forEach((navItem, i) => 
         navItem.classList.replace(`inactive-${i + 1}`, `active-${i + 1}`));
-        setTimeout(() => {
-            return navItems.forEach((navItem, i) => 
-            navItem.classList.replace(`active-${i + 1}`, `inactive-${i + 1}`));
-        }, 5000);
+    }
+}
+
+function retractMenu() {
+    if (nav1.classList.contains('active-1')) {
+    setTimeout(() => {
+		return navItems.forEach((navItem, i) =>
+		navItem.classList.replace(`active-${i + 1}`, `inactive-${i + 1}`));
+        }, 2500);
     }
 }
 
 menuLogo.addEventListener('mouseover', expandMenu);
+navMenu.addEventListener('mouseleave', retractMenu);

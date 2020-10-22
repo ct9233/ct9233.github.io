d53835690873c9769edb38dac27359a890e37066
Chris Twaddell <ct9233@gmail.com>
2020-10-21 21:25:35 -0700

    Image Placeholder and Flex Sections

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nav1 = document.getElementById('nav-1');
 const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const navMenu = document.getElementById('menu');
-const navItems = [nav1, nav2, nav3]
+const navItems = [nav1, nav2, nav3];
 
 function expandMenu() {
     if (nav1.classList.contains('inactive-1')) {
@@ -15,7 +15,7 @@ function expandMenu() {
 function retractMenu() {
     if (nav1.classList.contains('active-1')) {
     setTimeout(() => {
-		return navItems.forEach((navItem, i) =>
+		navItems.forEach((navItem, i) =>
 		navItem.classList.replace(`active-${i + 1}`, `inactive-${i + 1}`));
         }, 2500);
     }

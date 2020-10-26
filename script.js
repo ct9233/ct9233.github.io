c9e5a64eb0aaa18f758a2e111b7aa163bb5ba42c
Chris Twaddell <ct9233@gmail.com>
2020-10-26 00:29:47 -0700

    Menu Logo Animation plus Link Container Fixes

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ const nav3 = document.getElementById('nav-3');
 const navMenu = document.getElementById('menu');
 const navItems = [nav1, nav2, nav3];
 let timer;
+let borderTimer;
 
 function expandMenu() {
     menuLogo.classList.add('logo-active');
     if (nav1.classList.contains('inactive-1')) {
         navItems.forEach((navItem, i) => 
         navItem.classList.replace(`inactive-${i + 1}`, `active-${i + 1}`));
+        menuLogo.classList.remove('logo-pulse');
     } else {
         navItems.forEach((navItem, i) => {
         navItem.classList.add(`active-${i + 1}`);
+        menuLogo.classList.remove('logo-pulse');
         navItem.classList.remove('first-load');
         })
     }
@@ -26,9 +29,15 @@ function retractMenu() {
             navItem.classList.replace(`active-${i + 1}`, `inactive-${i + 1}`));
             menuLogo.classList.remove('logo-active');
         }, 2000);
+        borderTimer = setTimeout(() => menuLogo.classList.add('logo-pulse'), 2800)
     }
+}
+
+function clearTimers() {
+    clearTimeout(timer);
+    clearTimeout(borderTimer);
 }
 
 menuLogo.addEventListener('mouseover', expandMenu);
 navMenu.addEventListener('mouseleave', retractMenu);
-navMenu.addEventListener('mouseover', () => clearTimeout(timer));
+navMenu.addEventListener('mouseover', clearTimers);

7353be1a2402cf26f7a4f8e494c19280b25affb0
Chris Twaddell <ct9233@gmail.com>
2020-10-26 18:05:53 -0700

    Menu Depth, About Card, Email Function

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const nav2 = document.getElementById('nav-2');
 const nav3 = document.getElementById('nav-3');
 const navMenu = document.getElementById('menu');
 const navItems = [nav1, nav2, nav3];
-let timer;
+let expandTimer;
 let borderTimer;
 
 function expandMenu() {
@@ -24,7 +24,7 @@ function expandMenu() {
 
 function retractMenu() {
     if (nav1.classList.contains('active-1')) {
-        timer = setTimeout(() => {
+        expandTimer = setTimeout(() => {
             navItems.forEach((navItem, i) =>
             navItem.classList.replace(`active-${i + 1}`, `inactive-${i + 1}`));
             menuLogo.classList.remove('logo-active');
@@ -34,8 +34,19 @@ function retractMenu() {
 }
 
 function clearTimers() {
-    clearTimeout(timer);
+    clearTimeout(expandTimer);
     clearTimeout(borderTimer);
+}
+
+function sendEmail() {
+    const part1 = 'mai';
+    const part2 = 'lto:';
+    const part3 = 'ct9';
+    const part4 = '233@';
+    const part5 = 'gma';
+    const part6 = 'il.';
+    const part7 = 'com';
+    window.open(`${part1}${part2}${part3}${part4}${part5}${part6}${part7}`);
 }
 
 menuLogo.addEventListener('mouseover', expandMenu);
